a5b105277b0a4e21c4c48ed4575c2eeeff137500
Ali emre BÜLBÜL <aliemrebulbul@gmail.com>
2021-03-16 00:35:42 +0300

    güncellendi

---
+++ BinaYonetimi.Business/KullaniciBLL.cs
@@ -0,0 +1,26 @@
+﻿using BinaYonetimi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaYonetimi.Business
+{
+   public static class KullaniciBLL
+    {
+       static KullaniciDAL kullaniciDAL = new KullaniciDAL();
+
+
+        public static int kullaniciLogin(string kullaniciAdi, string sifre)
+        {
+            int durum = 0;
+            if (kullaniciAdi!="" && sifre!="")
+            {
+                durum = kullaniciDAL.kullaniciLogin(kullaniciAdi, sifre);
+            }
+            return durum;
+        }
+
+    }
+}
---
+++ BinaYonetimi.DAL/IGelenMesajDAL.cs
@@ -0,0 +1,14 @@
+﻿using BinaYonetimi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaYonetimi.DAL
+{
+   public interface IGelenMesajDAL:IRepository<GelenMesaj>
+    {
+
+    }
+}
